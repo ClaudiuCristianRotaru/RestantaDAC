16c088fd45c76331109ac4125b287864d5fff0db
Rotaru Claudiu <clayarname4@yahoo.com>
2022-06-09 21:19:39 +0300

    Everything

---
+++ app/src/main/java/com/example/testlicenta/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.testlicenta;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button but = findViewById(R.id.loginButton);
+        but.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity2.class);
+            intent.putExtra(EXTRA_MESSAGE, "agahaha");
+            startActivity(intent);
+        });
+
+    }
+}
